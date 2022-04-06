3f9bedbd491e9077730aaa9ba1324e51d0c9b05a
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-06 18:53:33 +0900

    [ADD] 0406 문자열 정수로 바꾸기

---
+++ src/p0406.java
@@ -0,0 +1,9 @@
+public class p0406 {
+    public static void main(String[] args){
+        System.out.println(solution("1234"));
+        System.out.println(solution("-1234"));
+    }
+    static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
